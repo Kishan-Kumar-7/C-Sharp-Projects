422f237c658b59111064eafb567903de819dc148
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2024-11-16 19:38:27 +0000

    update Student Form and Complited requirements. added Math Project

---
+++ Basic_C#_Program/MathAssigment/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Please Enter value for Math Oprations");
+Console.WriteLine("Please Enter First Value :");
+int value1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please Enter Second Value :");
+int value2 = Convert.ToInt32(Console.ReadLine());
+int addition = value1 + value2;
+int substract = value1 - value2;
+int multiply = value1 * value2;
+int divide = value1 / value2;
+int modulus = value1 % value2;
+Console.WriteLine("The Addition of Value is :" + addition);
+Console.WriteLine("The Addition of Value is :" + substract);
+Console.WriteLine("The Addition of Value is :" + multiply);
+Console.WriteLine("The Addition of Value is :" + divide);
+Console.WriteLine("The Addition of Value is :" + modulus);
