4f31a3abe616698acc044d857a019b697eb5d33e
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2024-11-22 14:44:49 +0000

    Created Turnery Oprator program

---
+++ Basic_C#_Program/TurneryOperator/TurneryOperator/Program.cs
@@ -0,0 +1,11 @@
+﻿// Title for programm
+Console.WriteLine("Turnery Oprator Programm\n");
+
+//Asking for user input
+Console.WriteLine("Please Enter your Age :");
+// taking string and convert in to integer and store in variable
+int uAge = Convert.ToInt32(Console.ReadLine());
+// compare integer to fixed value with turnery oprator and storing msg in uMsg variable
+string uMsg = uAge >= 20? "You are An Adult.":"You are not An Adult.";
+// Display the output to user
+Console.WriteLine(uMsg);
