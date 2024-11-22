f82a8d173adc9bbd4123823af15873d50b3cc5d9
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2024-11-22 11:45:35 +0000

    Created Project for DUI and tickets cheking for user.

---
+++ Basic_C#_Program/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,22 @@
+﻿// Title for programm to ensure user.
+Console.WriteLine("This Program Check For Driving Under Influence(DUI)\n");
+// asking for first condition user input
+Console.WriteLine("What is Your Age? :");
+// input take as string and convert in to integer and stored in variable
+int uAge = Convert.ToInt32(Console.ReadLine());
+// asking for second condition user input
+Console.WriteLine("Have you ever had a DUI? (true/false) :");
+// string input convert in to bool and store in variable
+bool uDUI = Convert.ToBoolean(Console.ReadLine());
+// asking for third condition user input
+Console.WriteLine("How Many Speeding Tickets Do You Have? :");
+// input take as string and convert in to integer and stored in variable
+int utick = Convert.ToInt32(Console.ReadLine());
+
+// create boolean variable and check conditions.
+// if age is more than 13 and DUI is false or tickets are lower than 3 then user is qualified for driving.
+bool quali = uAge > 15 && (uDUI == false ||  utick < 3);
+// output to user 
+Console.WriteLine("Are You Qualified to Drive? :"+quali);
+// to wait for user input for end program
+Console.ReadLine();
