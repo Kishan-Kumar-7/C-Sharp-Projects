2c400546f064e973642b6634f0437d847c286d49
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2024-12-08 18:23:23 +0000

    update on BlackJack Game

---
+++ Basic_C#_Program/MainMethodAssigment/MainMethodAssigment/Program.cs
@@ -0,0 +1,10 @@
+﻿using MainMethodAssigment;
+//creating math2 class
+math2 math2 = new math2();
+
+//call the first method with integer
+Console.WriteLine(math2.Multi(7));
+//call second method with decimal
+Console.WriteLine(math2.Multi(7.9m));
+//call third method with string
+Console.WriteLine(math2.Multi("7"));
---
+++ Basic_C#_Program/MainMethodAssigment/MainMethodAssigment/math2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssigment
+{
+    public class math2
+    {
+        //this mathod will take integer and return integer
+        public static int Multi(int x) { 
+            return x*7;
+        }
+        // this mathod will take decimal and return integer
+        public static int Multi(decimal x) { 
+            return Convert.ToInt32(x*7);
+        }
+        public static int Multi(string x) { 
+            int y = Convert.ToInt32(x);
+            return y*7;
+        }
+    }
+}
