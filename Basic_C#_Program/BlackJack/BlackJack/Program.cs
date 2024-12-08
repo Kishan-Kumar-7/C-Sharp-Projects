ccb3599796441aaa9b8920a645da3e08731764e1
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2024-12-08 17:16:54 +0000

    Shuffle update on BlackJack Game

--- Basic_C#_Program/BlackJack/BlackJack/Program.cs
+++ Basic_C#_Program/BlackJack/BlackJack/Program.cs
@@ -6,24 +6,30 @@ namespace BlackJack
         static void Main()
         {
             Deck deck = new Deck();
-            deck = Shuffle(deck);
+            int timeSuffled = 0;
+            deck = Shuffle(deck,out timeSuffled, 5);
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
-            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("The Deck Suffuled {0} Times.",timeSuffled);
         }
-        public static Deck Shuffle(Deck deck)
+        public static Deck Shuffle(Deck deck, out int timeCount, int times = 1)
         {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random();
-            while (deck.Cards.Count > 0)
+            timeCount = 0;
+            for (int i = 0; i < times; i++)
             {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
+                timeCount++;
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = TempList;
             }
-            deck.Cards = TempList;
             return deck;
         }
     }
--- Basic_C#_Program/BlackJack/BlackJack/obj/Debug/net8.0/BlackJack.AssemblyInfo.cs
+++ Basic_C#_Program/BlackJack/BlackJack/obj/Debug/net8.0/BlackJack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7880cf55da4226de73757e9655e38a3d20c783b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8384fc2f36d483c36773446b63d5e5a1c4173a07")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
