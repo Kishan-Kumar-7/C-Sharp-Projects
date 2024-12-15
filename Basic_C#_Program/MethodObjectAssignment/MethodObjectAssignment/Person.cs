b2d50e9894754575ee552d29ae026d5f837e0d2c
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2024-12-15 18:09:40 +0000

    created method object Assignment for inheritance and use of superclass

---
+++ Basic_C#_Program/MethodObjectAssignment/MethodObjectAssignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodObjectAssignment
+{   
+    // create public class person with two properties and one method
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // create method to pring full name on screen
+        public void SayName()
+        { 
+            string  FullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: "+FullName);
+        }
+    }
+}
---
+++ Basic_C#_Program/MethodObjectAssignment/MethodObjectAssignment/Program.cs
@@ -0,0 +1,8 @@
+﻿using MethodObjectAssignment;
+// using class employee with inheritance of class person
+Employee employee = new Employee();
+// assign values to class person
+employee.FirstName = "Sample";
+employee.LastName = "Student";
+// using method from superclass person
+employee.SayName();
