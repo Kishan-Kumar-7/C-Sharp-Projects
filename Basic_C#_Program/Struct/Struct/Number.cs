5afd059fde62f147ded2bea532aa4ce9c1113a39
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2025-02-01 17:06:22 +0000

    Creating Struct Project

---
+++ Basic_C#_Program/Struct/Struct/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    // creating structure Name Number
+    public struct Number
+    {
+        // Creating Decimal Variable name Amount
+        public Decimal Amount;
+    }
+}
---
+++ Basic_C#_Program/Struct/Struct/Program.cs
@@ -0,0 +1,7 @@
+﻿using Struct;
+// Creating New Object name Number
+Number number = new Number();
+// inisiating the value of amount 
+number.Amount = 25.55M;
+// Printing the value of amount.
+Console.WriteLine(number.Amount.ToString());
