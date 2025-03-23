6a0a3d74ee323129cf3c3343c566ab7b2723766d
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2025-03-23 21:13:45 +0000

    ASP.NET MVC With fremwork

---
+++ Basic_C#_Program/Scores/Scores/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Please Enter Your First Name: ");
+string firstName = Console.ReadLine();
+string date = DateTime.Today.ToShortDateString();
+String msg = $"Welcome Back {firstName}, Today is {date}.";
+Console.WriteLine(msg);
+string path = @"C:\Users\Kisha\Documents\Study\Github\C-Sharp-Projects\Basic_C#_Program\Scores\Scores\studentScores.txt";
+
+string[] lines = File.ReadAllLines(path);
+
+double tScore = 0.0;
+
+Console.WriteLine("Student Scores: ");
+foreach (string line in lines)
+{
+    Console.WriteLine(line);
+    double score = Convert.ToDouble(line);
+    tScore += score;
+}
+
+double avg = tScore / lines.Length;
+Console.WriteLine($"Total of {lines.Length} students scores is {tScore}. \tAverage Score :{avg}.");
