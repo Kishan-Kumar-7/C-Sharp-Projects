d5a70ca478eed3e57d5d76bdad9979812f4584ed
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2025-03-29 20:56:56 +0000

    Commit All Changes in Multiple Projects

--- Basic_C#_Program/PitMan/PitMan/Controllers/StudentsController.cs
+++ Basic_C#_Program/PitMan/PitMan/Controllers/StudentsController.cs
@@ -47,7 +47,7 @@ namespace PitMan.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "StudentFirstName,StudentLastName,DateOfBirth,Course")] Student student)
+        public ActionResult Create([Bind(Include = "StudentID,StudentFirstName,StudentLastName,DateOfBirth,Course")] Student student)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace PitMan.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "StudentFirstName,StudentLastName,DateOfBirth,Course")] Student student)
+        public ActionResult Edit([Bind(Include = "StudentID,StudentFirstName,StudentLastName,DateOfBirth,Course")] Student student)
         {
             if (ModelState.IsValid)
             {
